dd3d03b9576fbf08d6d7d1801cca8a0745a0acd5
Michal A <michal@pokryfka.net>
2020-07-07 21:52:33 +0800

    test: define precision of time comparison

--- Tests/AWSXRayRecorderTests/Utils/TimestampTests.swift
+++ Tests/AWSXRayRecorderTests/Utils/TimestampTests.swift
@@ -4,23 +4,33 @@ import XCTest
 
 final class TimestampTests: XCTestCase {
     func testTimestampAgainstFoundation() {
-        let before = Date().timeIntervalSince1970
-        let timestamp = Timestamp().secondsSinceEpoch
-        let after = Date().timeIntervalSince1970
-        XCTAssertLessThanOrEqual(Double(before), timestamp)
-        XCTAssertGreaterThanOrEqual(Double(after), timestamp)
+        let before = Date()
+        let timestamp = Timestamp()
+        let after = Date()
+        assert(timestamp: timestamp, after: after, before: before)
     }
 
     func testTimestampEncoding() {
-        let before = Date().timeIntervalSince1970
+        let before = Date()
         let timestamp = Timestamp()
-        let after = Date().timeIntervalSince1970
+        let after = Date()
         let encoder = JSONEncoder()
         let string = XCTAssertNoThrowResult(String(decoding: try encoder.encode(timestamp), as: UTF8.self))
         XCTAssertNotNil(string)
         let seconds = Double(string!)
         XCTAssertNotNil(seconds)
-        XCTAssertLessThanOrEqual(Double(before), seconds!)
-        XCTAssertGreaterThanOrEqual(Double(after), seconds!)
+        assert(seconds: seconds, after: after, before: before)
+    }
+
+    func assert(timestamp: Timestamp, after: Date, before: Date) {
+        assert(seconds: timestamp.secondsSinceEpoch, after: after, before: before)
+    }
+
+    func assert(seconds: Double?, after: Date, before: Date) {
+        let seconds = seconds ?? 0
+        let afterSeconds = after.timeIntervalSince1970
+        let beforeSeconds = before.timeIntervalSince1970
+        XCTAssertLessThanOrEqual(UInt(beforeSeconds * 1_000_000), UInt(seconds * 1_000_000))
+        XCTAssertGreaterThanOrEqual(UInt(afterSeconds * 1_000_000), UInt(seconds * 1_000_000))
     }
 }
