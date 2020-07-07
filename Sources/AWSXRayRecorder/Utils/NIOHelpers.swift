cfc65a93ee2d932cfbcbef36ea8e4401e16684c6
Michal A <michal@pokryfka.net>
2020-07-07 17:35:59 +0800

    test: test SocketAddress parsing

--- Sources/AWSXRayRecorder/Utils/NIOHelpers.swift
+++ Sources/AWSXRayRecorder/Utils/NIOHelpers.swift
@@ -1,17 +1,18 @@
 import NIO
 
 enum SocketAddressExtError: Error {
-    case failedToParseEndpointString(String)
+    case invalidEndpoint(String)
+    case invalidPort(String)
 }
 
 internal extension SocketAddress {
     init(string: String) throws {
         let ipPort = string.split(separator: ":")
-        guard
-            ipPort.count == 2,
-            let port = Int(ipPort[1])
-        else {
-            throw SocketAddressExtError.failedToParseEndpointString(string)
+        guard ipPort.count == 2 else {
+            throw SocketAddressExtError.invalidEndpoint(string)
+        }
+        guard let port = Int(ipPort[1]) else {
+            throw SocketAddressExtError.invalidPort(String(ipPort[1]))
         }
         let ipAddress = String(ipPort[0])
         try self.init(ipAddress: ipAddress, port: port)
---
+++ Tests/AWSXRayRecorderTests/Utils/NIOHelpersTests.swift
@@ -0,0 +1,80 @@
+import NIO
+import XCTest
+
+@testable import AWSXRayRecorder
+
+// XCTAssertNoThrow does not return the result
+private func XCTAssertNoThrowResult<T>(_ expression: @autoclosure () throws -> T, _ message: @autoclosure () -> String = "", file: StaticString = #file, line: UInt = #line) -> T? {
+    do {
+        return try expression()
+    } catch {
+        XCTFail(message(), file: file, line: line)
+        return nil
+    }
+}
+
+final class NIOHelpersTests: XCTestCase {
+    func testSocketAddressParsingPositive() {
+        let validEndpoints = [
+            ("127.0.0.1:2000", "127.0.0.1", 2000),
+            ("192.168.0.1:4000", "192.168.0.1", 4000),
+        ]
+        for (endpoint, ipAddress, port) in validEndpoints {
+            let address = XCTAssertNoThrowResult(try SocketAddress(string: endpoint))
+            XCTAssertNotNil(address)
+            XCTAssertEqual(ipAddress, address?.ipAddress)
+            XCTAssertEqual(port, address?.port)
+        }
+    }
+
+    func testSocketAddressParsingInvalidIpAddress() {
+        let invalidIp = [
+            "300.0.0.1:2000",
+            "168.0.1:4000",
+        ]
+        for endpoint in invalidIp {
+            let segments = endpoint.split(separator: ":")
+            let ipAddress = String(segments[0])
+            XCTAssertThrowsError(try SocketAddress(string: endpoint)) { error in
+                if case SocketAddressError.failedToParseIPString(let invalidValue) = error {
+                    XCTAssertEqual(invalidValue, ipAddress)
+                } else {
+                    XCTFail()
+                }
+            }
+        }
+    }
+
+    func testSocketAddressParsingNoPort() {
+        let noPort = [
+            "127.0.0.1",
+            "168.0.1",
+        ]
+        for endpoint in noPort {
+            XCTAssertThrowsError(try SocketAddress(string: endpoint)) { error in
+                if case SocketAddressExtError.invalidEndpoint(let invalidValue) = error {
+                    XCTAssertEqual(invalidValue, endpoint)
+                } else {
+                    XCTFail()
+                }
+            }
+        }
+    }
+
+    func testSocketAddressParsingInvalidPort() {
+        let invalidPort = [
+            "127.0.0.1:a",
+        ]
+        for endpoint in invalidPort {
+            let segments = endpoint.split(separator: ":")
+            let port = String(segments[1])
+            XCTAssertThrowsError(try SocketAddress(string: endpoint)) { error in
+                if case SocketAddressExtError.invalidPort(let invalidValue) = error {
+                    XCTAssertEqual(invalidValue, port)
+                } else {
+                    XCTFail()
+                }
+            }
+        }
+    }
+}
