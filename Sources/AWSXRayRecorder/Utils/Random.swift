ddded8b5aceeaf79f594acb636f5855669986afb
Michal A <michal@pokryfka.net>
2020-07-08 12:01:53 +0800

    refactor: cosmetic

--- Sources/AWSXRayRecorder/Utils/Random.swift
+++ Sources/AWSXRayRecorder/Utils/Random.swift
@@ -1,12 +1,10 @@
 internal extension String {
     /// - returns: A 64-bit identifier in 24 hexadecimal digits.
-    @inlinable
     static func random64() -> String {
         String(UInt64.random(in: UInt64.min ... UInt64.max) | 1 << 63, radix: 16, uppercase: false)
     }
 
     /// - returns: A 96-bit identifier in 24 hexadecimal digits.
-    @inlinable
     static func random96() -> String {
         String(UInt64.random(in: UInt64.min ... UInt64.max) | 1 << 63, radix: 16, uppercase: false)
             + String(UInt32.random(in: UInt32.min ... UInt32.max) | 1 << 31, radix: 16, uppercase: false)
--- Sources/AWSXRayRecorder/Utils/Timestamp.swift
+++ Sources/AWSXRayRecorder/Utils/Timestamp.swift
@@ -6,12 +6,10 @@ internal struct Timestamp: RawRepresentable {
     /// The number of seconds since the Unix epoch.
     var secondsSinceEpoch: Double { Double(Int64(bitPattern: rawValue)) / -1_000_000_000 }
 
-    @inlinable
     init?(rawValue: UInt64) {
         self.rawValue = rawValue
     }
 
-    @inlinable
     init() {
         rawValue = DispatchWallTime.now().rawValue
     }
