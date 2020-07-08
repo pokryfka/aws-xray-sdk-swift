6c60322d4b3b63b9fd87085f5a30ef4bfbbc9f7a
Michal A <michal@pokryfka.net>
2020-07-08 11:29:22 +0800

    chore: create script to run tests on linux

---
+++ scripts/test_linux.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+DOCKER_IMAGE=swift-lambda-builder
+
+docker inspect ${DOCKER_IMAGE} > /dev/null 2>&1 || exit 1
+
+docker run \
+  --rm \
+  --volume "$(pwd)/:/src" \
+  --workdir "/src/" \
+  ${DOCKER_IMAGE} \
+  swift test
