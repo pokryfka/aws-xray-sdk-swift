0e3f91beb3537f3dc589b74d6b5a3eb8a1905e20
pokryfka <michal@pokryfka.net>
2020-07-10 22:33:47 +0800

    chore: enable code coverage (#7)
    * chore: enable test discovery
    
    * test: fix testTimestampEncoding on Linux
    
    * chore: enable code coverage
    
    * chore: reduce verbosity in build jobs
    
    * chore: print swift version in build and test jobs
    
    * chore: run test-linux on swift 5.2 and 5.3 images
    
    * chore: ignore build and tests in codecov

--- Tests/AWSXRayRecorderTests/Utils/TimestampTests.swift
+++ Tests/AWSXRayRecorderTests/Utils/TimestampTests.swift
@@ -13,22 +13,23 @@ final class TimestampTests: XCTestCase {
     func testTimestampEncoding() {
         let before = Date()
         let timestamp = Timestamp()
+        // encoding timestamp fails on Linux with "Top-level Timestamp encoded as number JSON fragment."
+        let value = ["timestmap": timestamp]
         let after = Date()
         let encoder = JSONEncoder()
-        let string = XCTAssertNoThrowResult(String(decoding: try encoder.encode(timestamp), as: UTF8.self))
-        XCTAssertNotNil(string)
-        let seconds = Double(string!)
+        let string = String(decoding: try! encoder.encode(value), as: UTF8.self)
+        let parsedValue = string.dropFirst("{\"timestmap\":".count).dropLast()
+        let seconds = Double(parsedValue)
         XCTAssertNotNil(seconds)
-        assert(seconds: seconds, after: after, before: before)
+        assert(seconds: seconds!, after: after, before: before)
     }
 
     func assert(timestamp: Timestamp, after: Date, before: Date) {
         assert(seconds: timestamp.secondsSinceEpoch, after: after, before: before)
     }
 
-    func assert(seconds: Double?, after: Date, before: Date, decimals: UInt = 4) {
+    func assert(seconds: Double, after: Date, before: Date, decimals: UInt = 4) {
         let n = pow(10.0, Double(decimals))
-        let seconds = seconds ?? 0
         let afterSeconds = after.timeIntervalSince1970
         let beforeSeconds = before.timeIntervalSince1970
         XCTAssertLessThanOrEqual(UInt64(beforeSeconds * n), UInt64(seconds * n))
