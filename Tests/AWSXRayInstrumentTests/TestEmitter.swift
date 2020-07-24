7a1e1062113c51b587c41b34b4ef5ef3364904ec
Michal A <michal@pokryfka.net>
2020-07-25 01:38:17 +0800

    docs: add license headers

--- Tests/AWSXRayInstrumentTests/TestEmitter.swift
+++ Tests/AWSXRayInstrumentTests/TestEmitter.swift
@@ -1,10 +1,22 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the aws-xray-sdk-swift open source project
+//
+// Copyright (c) 2020 pokryfka and the aws-xray-sdk-swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 @testable import AWSXRayRecorder
 
 class TestEmitter: XRayEmitter {
     var segments = [XRayRecorder.Segment]()
 
     func send(_ segment: XRayRecorder.Segment) {
-        // TODO: flatten, append subsegments separately
         segments.append(segment)
     }
 
