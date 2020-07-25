6a9534e42379acc3d3dd20e5dee3a4884939107d
Michal A <michal@pokryfka.net>
2020-07-25 14:15:24 +0800

    refactor: remove dependency on lambda runtime
    Closes #30

--- Examples/Package.swift
+++ Examples/Package.swift
@@ -15,8 +15,9 @@ let package = Package(
     dependencies: [
         .package(name: "aws-xray-sdk-swift", path: ".."),
         .package(url: "https://github.com/swift-aws/aws-sdk-swift.git", .upToNextMinor(from: "5.0.0-alpha.5")),
-        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", .upToNextMajor(from: "0.2.0")),
         .package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", .upToNextMajor(from: "0.2.0")),
+        .package(url: "https://github.com/Flight-School/AnyCodable.git", .upToNextMajor(from: "0.3.0")),
     ],
     targets: [
         .target(
@@ -37,7 +38,7 @@ let package = Package(
             name: "AWSXRayRecorderExampleLambda",
             dependencies: [
                 .product(name: "AWSXRayRecorder", package: "aws-xray-sdk-swift"),
-                .product(name: "AWSXRayRecorderLambda", package: "aws-xray-sdk-swift"),
+                .product(name: "AnyCodable", package: "AnyCodable"),
                 .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-runtime"),
                 .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
             ]
--- Package.swift
+++ Package.swift
@@ -9,14 +9,12 @@ let package = Package(
     ],
     products: [
         .library(name: "AWSXRayRecorder", targets: ["AWSXRayRecorder"]),
-        .library(name: "AWSXRayRecorderLambda", targets: ["AWSXRayRecorderLambda"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.17.0")),
         .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/Flight-School/AnyCodable.git", .upToNextMajor(from: "0.3.0")),
         .package(name: "swift-baggage-context", url: "https://github.com/slashmo/gsoc-swift-baggage-context.git", .upToNextMinor(from: "0.1.0")),
-        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", .upToNextMajor(from: "0.2.0")),
     ],
     targets: [
         .target(
@@ -26,13 +24,6 @@ let package = Package(
                 .product(name: "Logging", package: "swift-log"),
                 .product(name: "AnyCodable", package: "AnyCodable"),
                 .product(name: "Baggage", package: "swift-baggage-context"),
-            ]
-        ),
-        .target(
-            name: "AWSXRayRecorderLambda",
-            dependencies: [
-                .byName(name: "AWSXRayRecorder"),
-                .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
             ]
         ),
         .testTarget(
