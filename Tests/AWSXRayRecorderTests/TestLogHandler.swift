4bec4e44ddb2c9a2506451d0145ea8c277a29f2b
Michal A <michal@pokryfka.net>
2020-07-31 14:22:42 +0800

    fix: update license headers

--- Tests/AWSXRayRecorderTests/TestLogHandler.swift
+++ Tests/AWSXRayRecorderTests/TestLogHandler.swift
@@ -1,9 +1,15 @@
+//===----------------------------------------------------------------------===//
 //
-//  File.swift
+// This source file is part of the aws-xray-sdk-swift open source project
 //
+// Copyright (c) 2020 pokryfka and the aws-xray-sdk-swift project authors
+// Licensed under Apache License v2.0
 //
-//  Created by Michał A on 2020/7/31.
+// See LICENSE.txt for license information
 //
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 import Foundation
 import Logging
