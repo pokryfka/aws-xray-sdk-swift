f7b0952462ef38132a19e9e4038c9d6dfd3ceedc
Michal A <michal@pokryfka.net>
2020-08-01 16:50:46 +0800

    fix: prettify output the log emitter

--- Sources/AWSXRayTesting/FoundationEncoding.swift
+++ Sources/AWSXRayTesting/FoundationEncoding.swift
@@ -25,6 +25,7 @@ private extension JSONEncoder {
 internal enum FoundationJSON {
     public static let encoding: XRayRecorder.Segment.Encoding = {
         let jsonEncoder = JSONEncoder()
+        jsonEncoder.outputFormatting = .prettyPrinted
         return XRayRecorder.Segment.Encoding { segment in
             try jsonEncoder.encode(segment) as String
         }
