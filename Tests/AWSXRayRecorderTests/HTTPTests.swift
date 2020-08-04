a18289cce87a711ba2ef9da992abd24db4d44e01
Michal A <dev@pokryfka.net>
2020-08-04 08:33:23 +0800

    test: update segment tests

--- Tests/AWSXRayRecorderTests/HTTPTests.swift
+++ Tests/AWSXRayRecorderTests/HTTPTests.swift
@@ -22,7 +22,7 @@ private typealias Namespace = XRayRecorder.Segment.Namespace
 private typealias HTTP = XRayRecorder.Segment.HTTP
 
 final class HTTPTests: XCTestCase {
-    func testCreatingAWSRequest() {
+    func testRecordingAWSRequest() {
         let recorder = XRayRecorder(emitter: XRayNoOpEmitter())
         let context = TraceContext(traceId: .init(), sampled: true)
         let segment = recorder.beginSegment(name: UUID().uuidString, context: context)
@@ -38,7 +38,7 @@ final class HTTPTests: XCTestCase {
         XCTAssertEqual(Namespace.aws, segment._test_namespace)
     }
 
-    func testCreatingRemoteRequest() {
+    func testRecordingRemoteRequest() {
         let recorder = XRayRecorder(emitter: XRayNoOpEmitter())
         let context = TraceContext(traceId: .init(), sampled: true)
         let segment = recorder.beginSegment(name: UUID().uuidString, context: context)
@@ -54,7 +54,7 @@ final class HTTPTests: XCTestCase {
         XCTAssertEqual(Namespace.remote, segment._test_namespace)
     }
 
-    func testCreatingResponse() {
+    func testRecordingResponse() {
         let recorder = XRayRecorder(emitter: XRayNoOpEmitter())
         let context = TraceContext(traceId: .init(), sampled: true)
         let segment = recorder.beginSegment(name: UUID().uuidString, context: context)
@@ -65,6 +65,69 @@ final class HTTPTests: XCTestCase {
         segment.setHTTPResponse(status: status, contentLength: contentLength)
         let response = HTTP.Response(status: status, contentLength: contentLength)
         XCTAssertEqual(response, segment._test_http.response)
+    }
+
+    func testRecordingClientError() {
+        let recorder = XRayRecorder(emitter: XRayNoOpEmitter())
+        let context = TraceContext(traceId: .init(), sampled: true)
+        let segment = recorder.beginSegment(name: UUID().uuidString, context: context)
+
+        let status: UInt = 404
+        let contentLength: UInt = UInt.random(in: 0 ... UInt.max)
+
+        segment.setHTTPResponse(status: status, contentLength: contentLength)
+        let response = HTTP.Response(status: status, contentLength: contentLength)
+        XCTAssertEqual(response, segment._test_http.response)
+
+        var error: Bool?
+        var throttle: Bool?
+        var fault: Bool?
+        segment._test_error(error: &error, throttle: &throttle, fault: &fault)
+        XCTAssertEqual(true, error)
+        XCTAssertNil(throttle)
+        XCTAssertNil(fault)
+    }
+
+    func testRecordingThrottleError() {
+        let recorder = XRayRecorder(emitter: XRayNoOpEmitter())
+        let context = TraceContext(traceId: .init(), sampled: true)
+        let segment = recorder.beginSegment(name: UUID().uuidString, context: context)
+
+        let status: UInt = 429
+        let contentLength: UInt = UInt.random(in: 0 ... UInt.max)
+
+        segment.setHTTPResponse(status: status, contentLength: contentLength)
+        let response = HTTP.Response(status: status, contentLength: contentLength)
+        XCTAssertEqual(response, segment._test_http.response)
+
+        var error: Bool?
+        var throttle: Bool?
+        var fault: Bool?
+        segment._test_error(error: &error, throttle: &throttle, fault: &fault)
+        XCTAssertEqual(true, error)
+        XCTAssertEqual(true, throttle)
+        XCTAssertNil(fault)
+    }
+
+    func testRecordingServerError() {
+        let recorder = XRayRecorder(emitter: XRayNoOpEmitter())
+        let context = TraceContext(traceId: .init(), sampled: true)
+        let segment = recorder.beginSegment(name: UUID().uuidString, context: context)
+
+        let status: UInt = 500
+        let contentLength: UInt = UInt.random(in: 0 ... UInt.max)
+
+        segment.setHTTPResponse(status: status, contentLength: contentLength)
+        let response = HTTP.Response(status: status, contentLength: contentLength)
+        XCTAssertEqual(response, segment._test_http.response)
+
+        var error: Bool?
+        var throttle: Bool?
+        var fault: Bool?
+        segment._test_error(error: &error, throttle: &throttle, fault: &fault)
+        XCTAssertNil(error)
+        XCTAssertNil(throttle)
+        XCTAssertEqual(true, fault)
     }
 
     func testLoggingErrors() {
@@ -85,7 +148,7 @@ final class HTTPTests: XCTestCase {
 import NIOHTTP1
 
 final class HTTPNIOTests: XCTestCase {
-    func testCreatingAWSRequestWithHTTPMethod() {
+    func testRecordingAWSRequestWithHTTPMethod() {
         let recorder = XRayRecorder(emitter: XRayNoOpEmitter())
         let context = TraceContext(traceId: .init(), sampled: true)
         let segment = recorder.beginSegment(name: UUID().uuidString, context: context)
@@ -101,7 +164,7 @@ final class HTTPNIOTests: XCTestCase {
         XCTAssertEqual(Namespace.aws, segment._test_namespace)
     }
 
-    func testCreatingRemoteRequestWithHTTPRequestHead() {
+    func testRecordingRemoteRequestWithHTTPRequestHead() {
         let recorder = XRayRecorder(emitter: XRayNoOpEmitter())
         let context = TraceContext(traceId: .init(), sampled: true)
         let segment = recorder.beginSegment(name: UUID().uuidString, context: context)
@@ -121,7 +184,7 @@ final class HTTPNIOTests: XCTestCase {
         XCTAssertEqual(Namespace.remote, segment._test_namespace)
     }
 
-    func testCreatingResponse() {
+    func testRecordingResponseWithHTTPResponseHead() {
         let recorder = XRayRecorder(emitter: XRayNoOpEmitter())
         let context = TraceContext(traceId: .init(), sampled: true)
         let segment = recorder.beginSegment(name: UUID().uuidString, context: context)
@@ -129,6 +192,16 @@ final class HTTPNIOTests: XCTestCase {
         let responseHead = HTTPResponseHead(version: .init(major: 1, minor: 1), status: .ok)
 
         segment.setHTTPResponse(responseHead)
+        let response = HTTP.Response(status: HTTPResponseStatus.ok.code)
+        XCTAssertEqual(response, segment._test_http.response)
+    }
+
+    func testRecordingResponseWithHTTPResponseStatus() {
+        let recorder = XRayRecorder(emitter: XRayNoOpEmitter())
+        let context = TraceContext(traceId: .init(), sampled: true)
+        let segment = recorder.beginSegment(name: UUID().uuidString, context: context)
+
+        segment.setHTTPResponse(status: .ok)
         let response = HTTP.Response(status: HTTPResponseStatus.ok.code)
         XCTAssertEqual(response, segment._test_http.response)
     }
--- Tests/AWSXRayRecorderTests/SegmentExceptionTests.swift
+++ Tests/AWSXRayRecorderTests/SegmentExceptionTests.swift
@@ -20,17 +20,19 @@ private typealias Segment = XRayRecorder.Segment
 private typealias SegmentError = XRayRecorder.SegmentError
 
 final class SegmentExceptionTests: XCTestCase {
-    func testRecordingExceptionsWithMessageAndType() {
-        let segment = Segment(id: .init(), name: UUID().uuidString, context: .init(), baggage: .init())
+    func testRecordingExceptions() {
+        let segment = Segment(id: .init(), name: UUID().uuidString, context: .init())
+        XCTAssertEqual(0, segment._test_exceptions.count)
 
         let messageWithType = (UUID().uuidString, UUID().uuidString)
         segment.addException(message: messageWithType.0, type: messageWithType.1)
+        XCTAssertEqual(1, segment._test_exceptions.count)
 
         let messageWithoutType = UUID().uuidString
         segment.addException(message: messageWithoutType)
+        XCTAssertEqual(2, segment._test_exceptions.count)
 
-        let exceptions = segment.exceptions
-        XCTAssertEqual(2, exceptions.count)
+        let exceptions = segment._test_exceptions
         XCTAssertEqual(messageWithType.0, exceptions[0].message)
         XCTAssertEqual(messageWithType.1, exceptions[0].type)
         XCTAssertEqual(messageWithoutType, exceptions[1].message)
@@ -38,7 +40,8 @@ final class SegmentExceptionTests: XCTestCase {
     }
 
     func testRecordingErrors() {
-        let segment = Segment(id: .init(), name: UUID().uuidString, context: .init(), baggage: .init())
+        let segment = Segment(id: .init(), name: UUID().uuidString, context: .init())
+        XCTAssertEqual(0, segment._test_exceptions.count)
 
         enum TestError: Error {
             case test1
@@ -46,13 +49,43 @@ final class SegmentExceptionTests: XCTestCase {
         }
 
         segment.addError(TestError.test1)
+        XCTAssertEqual(1, segment._test_exceptions.count)
         segment.addError(TestError.test2)
+        XCTAssertEqual(2, segment._test_exceptions.count)
 
-        let exceptions = segment.exceptions
-        XCTAssertEqual(2, exceptions.count)
+        let exceptions = segment._test_exceptions
         XCTAssertEqual("test1", exceptions[0].message) // may be a bit different
         XCTAssertNil(exceptions[0].type)
         XCTAssertEqual("test2", exceptions[1].message) // may be a bit different
         XCTAssertNil(exceptions[1].type)
+    }
+
+    func testPropagatingErrorsToParent() {
+        enum ExampleError: Error {
+            case test
+        }
+
+        let recorder = XRayRecorder(emitter: XRayNoOpEmitter())
+        recorder.segment(name: "Segment 2", context: .init()) { segment in
+            XCTAssertEqual(0, segment._test_exceptions.count)
+
+            try? segment.subsegment(name: "Subsegment 2.1") { segment in
+                XCTAssertEqual(0, segment._test_exceptions.count)
+
+                _ = segment.subsegment(name: "Subsegment 2.1.1 with Result") { _ -> String in
+                    usleep(100_000)
+                    return "Result"
+                }
+                try segment.subsegment(name: "Subsegment 2.1.2 with Error") { _ in
+                    usleep(200_000)
+                    throw ExampleError.test
+                }
+
+                XCTAssertEqual(1, segment._test_exceptions.count)
+            }
+
+            // note `try?`
+            XCTAssertEqual(0, segment._test_exceptions.count)
+        }
     }
 }
