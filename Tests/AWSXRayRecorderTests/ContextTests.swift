ee82ab18e0d8229b516d36abc0185c68b8d6d61e
pokryfka <dev@pokryfka.net>
2020-08-15 11:03:30 +0800

    chore: update swift-baggage-context to 0.2.0, test context propagation (#77)
    * chore: update swift-baggage-context to 0.2.0
    
    Closes #72
    
    * test: test context propagation
    
    * docs: update context propagation example

--- Tests/AWSXRayRecorderTests/ContextTests.swift
+++ Tests/AWSXRayRecorderTests/ContextTests.swift
@@ -17,9 +17,9 @@ import XCTest
 
 @testable import AWSXRayRecorder
 
-private typealias TraceContext = XRayRecorder.TraceContext
-
 final class ContextTests: XCTestCase {
+    private typealias TraceContext = XRayRecorder.TraceContext
+
     func testMissingContext() {
         let logHandler = TestLogHandler()
         let logger = Logger(label: "test", factory: { _ in logHandler })
@@ -28,5 +28,48 @@ final class ContextTests: XCTestCase {
         let baggage = BaggageContext()
         _ = recorder.beginSegment(name: UUID().uuidString, baggage: baggage)
         XCTAssertEqual(1, logHandler.errorMessages.count)
+    }
+
+    func testContextPropagation() {
+        enum TestKey: BaggageContextKey {
+            typealias Value = String
+        }
+
+        let recorder = XRayRecorder(emitter: XRayNoOpEmitter())
+        var baggage = BaggageContext()
+        let context = XRayContext()
+        baggage.xRayContext = context
+        let testValue = UUID().uuidString
+        baggage[TestKey] = testValue
+
+        let segment = recorder.beginSegment(name: "Segment 1", baggage: baggage)
+        XCTAssertNotEqual(context.parentId, segment.id)
+        XCTAssertEqual(context.traceId, segment._test_traceId)
+        XCTAssertEqual(context.parentId, segment._test_parentId)
+        XCTAssertEqual(context.isSampled, segment.isSampled)
+        XCTAssertEqual(context.traceId, segment.baggage.xRayContext?.traceId)
+        XCTAssertEqual(segment.id, segment.baggage.xRayContext?.parentId)
+        XCTAssertEqual(context.isSampled, segment.baggage.xRayContext?.isSampled)
+        XCTAssertEqual(testValue, segment.baggage[TestKey])
+
+        let subsegment = recorder.beginSegment(name: "Subsegment 1.1", baggage: segment.baggage)
+        XCTAssertNotEqual(segment.id, subsegment.id)
+        XCTAssertEqual(context.traceId, subsegment._test_traceId)
+        XCTAssertEqual(segment.id, subsegment._test_parentId)
+        XCTAssertEqual(context.isSampled, subsegment.isSampled)
+        XCTAssertEqual(context.traceId, subsegment.baggage.xRayContext?.traceId)
+        XCTAssertEqual(subsegment.id, subsegment.baggage.xRayContext?.parentId)
+        XCTAssertEqual(context.isSampled, subsegment.baggage.xRayContext?.isSampled)
+        XCTAssertEqual(testValue, subsegment.baggage[TestKey])
+
+        let subsegment2 = segment.beginSubsegment(name: "Subsegment 1.2")
+        XCTAssertNotEqual(segment.id, subsegment2.id)
+        XCTAssertEqual(context.traceId, subsegment2._test_traceId)
+        XCTAssertEqual(segment.id, subsegment2._test_parentId)
+        XCTAssertEqual(context.isSampled, subsegment2.isSampled)
+        XCTAssertEqual(context.traceId, subsegment.baggage.xRayContext?.traceId)
+        XCTAssertEqual(subsegment2.id, subsegment2.baggage.xRayContext?.parentId)
+        XCTAssertEqual(context.isSampled, subsegment2.baggage.xRayContext?.isSampled)
+        XCTAssertEqual(testValue, subsegment2.baggage[TestKey])
     }
 }
--- Tests/AWSXRayRecorderTests/NoOpRecorderTests.swift
+++ Tests/AWSXRayRecorderTests/NoOpRecorderTests.swift
@@ -11,6 +11,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+import Baggage
 import XCTest
 
 @testable import AWSXRayRecorder
@@ -19,7 +20,9 @@ final class NoOpRecorderTests: XCTestCase {
     func testCreatingSegments() {
         let recorder = XRayNoOpRecorder()
         XCTAssertTrue(recorder.beginSegment(name: UUID().uuidString, context: .init()) is XRayRecorder.NoOpSegment)
+        XCTAssertFalse(recorder.beginSegment(name: UUID().uuidString, context: .init()).isSampled)
         XCTAssertTrue(recorder.beginSegment(name: UUID().uuidString, baggage: .init()) is XRayRecorder.NoOpSegment)
+        XCTAssertFalse(recorder.beginSegment(name: UUID().uuidString, baggage: .init()).isSampled)
     }
 
     func testFlushing() {
@@ -40,5 +43,48 @@ final class NoOpRecorderTests: XCTestCase {
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1)
+    }
+
+    func testContextPropagation() {
+        enum TestKey: BaggageContextKey {
+            typealias Value = String
+        }
+
+        let recorder = XRayNoOpRecorder()
+        var baggage = BaggageContext()
+        let context = XRayContext()
+        baggage.xRayContext = context
+        let testValue = UUID().uuidString
+        baggage[TestKey] = testValue
+
+        let segment = recorder.beginSegment(name: "Segment 1", baggage: baggage)
+        XCTAssertNotEqual(context.parentId, segment.id)
+        XCTAssertEqual(context.traceId, segment._test_traceId)
+        XCTAssertEqual(context.parentId, segment._test_parentId)
+        XCTAssertFalse(segment.isSampled)
+        XCTAssertEqual(context.traceId, segment.baggage.xRayContext?.traceId)
+        XCTAssertEqual(segment.id, segment.baggage.xRayContext?.parentId)
+        XCTAssertEqual(context.isSampled, segment.baggage.xRayContext?.isSampled)
+        XCTAssertEqual(testValue, segment.baggage[TestKey])
+
+        let subsegment = recorder.beginSegment(name: "Subsegment 1.1", baggage: segment.baggage)
+        XCTAssertNotEqual(segment.id, subsegment.id)
+        XCTAssertEqual(context.traceId, subsegment._test_traceId)
+        XCTAssertEqual(segment.id, subsegment._test_parentId)
+        XCTAssertFalse(subsegment.isSampled)
+        XCTAssertEqual(context.traceId, subsegment.baggage.xRayContext?.traceId)
+        XCTAssertEqual(subsegment.id, subsegment.baggage.xRayContext?.parentId)
+        XCTAssertEqual(context.isSampled, subsegment.baggage.xRayContext?.isSampled)
+        XCTAssertEqual(testValue, subsegment.baggage[TestKey])
+
+        let subsegment2 = segment.beginSubsegment(name: "Subsegment 1.2")
+        XCTAssertNotEqual(segment.id, subsegment2.id)
+        XCTAssertEqual(context.traceId, subsegment2._test_traceId)
+        XCTAssertEqual(segment.id, subsegment2._test_parentId)
+        XCTAssertFalse(subsegment2.isSampled)
+        XCTAssertEqual(context.traceId, subsegment.baggage.xRayContext?.traceId)
+        XCTAssertEqual(subsegment2.id, subsegment2.baggage.xRayContext?.parentId)
+        XCTAssertEqual(context.isSampled, subsegment2.baggage.xRayContext?.isSampled)
+        XCTAssertEqual(testValue, subsegment2.baggage[TestKey])
     }
 }
